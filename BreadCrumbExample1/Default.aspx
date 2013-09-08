b987e691ff724459999a20ed88dc4e80691922b0
adrianillo <elcorreillodeadrianillo@gmail.com>
2013-09-08 21:34:43 +0200

    v.1

---
+++ BreadCrumbExample1/Default.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ejemplo1._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <script src="js/jquery.js" type="text/javascript"></script>    
+    <script src="js/mootools-core.js" type="text/javascript"></script>
+    <script src="js/mootools-more.js" type="text/javascript"></script>  
+    <script src="js/breadcrumb.js" type="text/javascript"></script>
+    <link href="css/breadcrumb.css" rel="stylesheet" type="text/css" />    
+    <title>Ejemplo 1</title>
+    <script type="text/javascript">
+     
+    
+    
+var bb;    
+var i=1;
+
+$(document).ready(function() {
+
+ bb=new jQuery.breadcrumb("contentbreadcrumbs",
+            {
+                //imgarrow:'img/arrow.png', //ruta para la imagen de la flecha entre crrums
+                classarrow:'arrowcrum', // clase de estilo para la flecha entre crrums
+                autocrum:true, // si es true  borra los crum hasta el clicado
+                classcrum:'crumclass', // clase de estilo para cada crum
+                classbread:'breadcrumbbs', // clase de estilo para cada el contenedor de los crum
+                dinamic: true,     // si es  true al hacer clik en el elemento realiza la función con los parametros definas en cada crum
+                crumclick: function (op,o) {          //función que realiza el hacer clik en cada crum si es el parametro anterior es dinamic                     
+                     if(op!=null)
+                        if(op.functioncrum!=null)
+                            op.functioncrum(this,op,o,op.args.id,op.args.texto,1,2,3);                       
+                }      
+            }
+        );        
+  
+});
+
+
+
+function add()
+{
+     var newitem=new jQuery.itembread("hola",
+     function (a,b,c,d,e,f)
+     {
+        alert('hola, 1º argumento opcional: '+d+' 2º argumento opcional: '+e);
+     },
+     {id:i,texto:'este es el segundo parametro opcional'}); // 1ºarg: texto a mostrar, 2º arg: parametro functioncrum, string , 3º arg: opciones para cada crum, aquí puedes poner los argumentos que quieras
+     
+     bb.addcrum(newitem);
+     i++;
+}
+
+
+function remove()
+{ 
+    bb.removecrum();
+}
+function change()
+{   
+    var newitem=new jQuery.itembread("Cambio menú",
+    function (a,b,c,d,e,f)
+    {
+          alert('Menú cambiado, 1º argumento opcional: '+d+' 2º argumento opcional: '+e);
+    },
+    {id:i,texto:'este es el segundo parametro opcional del menu cambiado.'}); 
+    bb.changecrum(newitem);
+    i++;
+}
+    
+    
+
+    
+    </script>
+    
+</head>
+<body >
+
+    <form id="form1" runat="server">
+    <div>
+     <div id="tuts">
+    <div class="tuts_inner">
+    		<div id='contentbreadcrumbs' class="breadcrumbs">    							
+
+<div class="inner-content">   </div>
+    </div>
+        </div>
+        <br />
+        <br />
+        <input type="button" value="add" onclick="add()" />        
+        <input type="button" value="remove" onclick="remove()" />
+        <input type="button" value="cambio" onclick="change()" />        
+    </form>
+    <div id="contentexec"></div>
+</body>
+</html>
---
+++ BreadCrumbExample1/js/breadcrumb.js
@@ -0,0 +1,117 @@
+﻿//requiere jquery, mootools-core.js y mootools-core.js
+jQuery.extend({
+    breadcrumb: function (selector, o) {
+        this.s = o;
+        mee=this;
+        if (o.dinamic != null)
+            this.dinamic = o.dinamic;
+        else
+            this.dinamic = false;
+            
+        if (o.classcrum != null)
+            this.classcrum = o.classcrum;
+        else
+            this.classcrum = '';
+            
+        if (o.classbread != null)
+            this.classbread = o.classbread;
+        else
+            this.classbread = '';            
+            
+        if (o.imgarrow != null)
+            this.imgarrow = o.imgarrow;
+        else
+            this.imgarrow = '';
+            
+       if (o.classarrow != null)
+            this.classarrow = o.classarrow;
+        else
+            this.classarrow = '';
+            
+       if (o.autocrum != null)
+            this.autocrum = o.autocrum;
+        else
+            this.autocrum = false;            
+            
+          if(o.classcrum!= null)
+            $(document.getElementById(selector)).addClass(o.classbread);
+            
+        listitembread = [];
+        content = $("#" + selector);
+
+        paintbreadcrumb = function () {
+            $(content).html('');
+            $(listitembread).each(function (i, oo) {
+            
+                var aa = new Element('a')
+                .inject(document.getElementById(selector));
+                
+                if(o.classcrum!= null)
+                    $(aa).addClass(o.classcrum); 
+                    
+                if(o.imgarrow!= null)
+                {
+                    $(aa).text(oo.textitem );
+                     if(listitembread.length-1>i)
+                     {                         
+                         var im = new Element('img');
+                         im.src=o.imgarrow;
+                         if(o.classarrow!= null)
+                              $(im).addClass(o.classarrow); 
+                         im.inject(aa);
+                     }
+                  
+                }                    
+                else
+                    $(aa).text(oo.textitem );
+                 
+                $(aa).bind('click', function () {
+                   $.crumclick(o,aa,oo,i);
+                });
+                                
+            });
+        };
+        this.addcrum = function (l) {
+            listitembread.push(l);
+            paintbreadcrumb();
+        };
+        this.removecrum = function () {
+            listitembread.pop();
+            paintbreadcrumb();
+        };
+        this.changecrum = function (l) {
+            this.removecrumwithoutpaint();
+            this.addcrum(l)
+        };
+        this.removecrumwithoutpaint = function () {
+            listitembread.pop();
+        };
+        this.removeto=function (i)
+        {
+        var to=listitembread.length-i;
+            for(var j=1;j<to;j++)
+                this.removecrumwithoutpaint();
+            paintbreadcrumb();
+        }
+
+    },
+    itembread: function (textitem,functioncrum,args) {
+        this.textitem = textitem;
+        this.functioncrum = function (ooo,ele,oo,i) {};
+        if(functioncrum!=null)
+            this.functioncrum =functioncrum;
+        this.args=args;
+    },
+    crumclick: function (ooo,ele,oo,i) {
+        s = jQuery.extend({}, jQuery.ajaxSettings, ooo);
+        
+        if(ooo.autocrum)
+            mee.removeto(i);
+       
+        if (s.crumclick)
+            s.crumclick(oo,ele);
+    }
+
+
+
+});
